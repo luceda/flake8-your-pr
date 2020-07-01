f2c91d674990c037c041fa531281f5148f8a6918
DennisVVV <dennis@lucedaphotonics.be>
2020-07-01 10:23:46 +0200

    test - remove grep

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -52,7 +52,7 @@ main() {
         git diff \
             --name-only \
             --diff-filter=AM \
-            "$BASE_COMMIT" | grep '\.py$' | tr '\n' ' '
+            "$BASE_COMMIT" | tr '\n' ' '
     )
 
     # Feed to flake8 which will return the output in json format.
